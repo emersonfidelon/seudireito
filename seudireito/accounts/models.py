4aa7c8d679906b5d1e1608dd04ed68af1dff718f
Emerson Santana <emersonfidelon@gmail.com>
2018-02-09 23:00:39 -0300

    create lawyer and company profiles

--- seudireito/accounts/models.py
+++ seudireito/accounts/models.py
@@ -1,10 +1,8 @@
 import re
 
 from django.db import models
-from django.core import validators
 from django.utils import timezone
 from django.core.mail import send_mail
-from django.utils.http import urlquote
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.conf import settings
@@ -12,12 +10,10 @@ from django.conf import settings
 
 class UserManager(BaseUserManager):
 
-    def _create_user(self, username, email, password, is_staff, is_superuser, **extra_fields):
+    def _create_user(self, email, password, is_staff, is_superuser, **extra_fields):
         now = timezone.now()
-        if not username:
-          raise ValueError(_('The given username must be set'))
         email = self.normalize_email(email)
-        user = self.model(username=username, email=email,
+        user = self.model(email=email,
                  is_staff=is_staff, is_active=True,
                  is_superuser=is_superuser, last_login=now,
                  date_joined=now, **extra_fields)
@@ -25,13 +21,11 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_user(self, username, email=None, password=None, **extra_fields):
-        return self._create_user(username, email, password, False, False,
-                 **extra_fields)
+    def create_user(self, email=None, password=None, **extra_fields):
+        return self._create_user(email, password, False, False, **extra_fields)
 
-    def create_superuser(self, username, email, password, **extra_fields):
-        user=self._create_user(username, email, password, True, True,
-                 **extra_fields)
+    def create_superuser(self, email, password, **extra_fields):
+        user=self._create_user(email, password, True, True, **extra_fields)
         user.is_active=True
         user.save(using=self._db)
         return user
@@ -39,41 +33,52 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser, PermissionsMixin):
 
-    username = models.CharField(_('username'), max_length=15, unique=True,
-        help_text=_('Required. 15 characters or fewer. Letters, \
-                    numbers and @/./+/-/_ characters'),
-        validators=[
-                    validators.RegexValidator(
-                                            re.compile('^[\w.@+-]+$'),
-                                            _('Enter a valid username.'),
-                                            _('invalid'))])
-    first_name = models.CharField(_('first name'), max_length=30)
-    last_name = models.CharField(_('last name'), max_length=30)
+    name = models.CharField(_('name'), max_length=100)
     email = models.EmailField(_('email address'), max_length=255, unique=True)
-    is_staff = models.BooleanField(_('staff status'), default=False,
-        help_text=_('Designates whether the user can log into this admin site.'))
-    is_active = models.BooleanField(_('active'), default=True,
-        help_text=_('Designates whether this user should be treated as active. \
-                    Unselect this instead of deleting accounts.'))
+    is_company = models.BooleanField(_('company'), default=False)
+    is_lawyer = models.BooleanField(_('lawyer'), default=False)
+    
+    is_staff = models.BooleanField(_('staff status'), default=False, help_text=_('Designates whether the user can log into this admin site.'))
+    is_active = models.BooleanField(_('active'), default=True, help_text=_('Designates whether this user should be treated as active. Unselect this instead of deleting accounts.'))
     date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
-    is_trusty = models.BooleanField(_('trusty'), default=False,
-        help_text=_('Designates whether this user has confirmed his account.'))
+    is_trusty = models.BooleanField(_('trusty'), default=False, help_text=_('Designates whether this user has confirmed his account.'))
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+    REQUIRED_FIELDS = ['name']
 
     objects = UserManager()
 
     class Meta:
         verbose_name = _('user')
         verbose_name_plural = _('users')
+    
+    def get_lawyer_profile(self):
+        lawyer_profile = None
+        if hasattr(self, 'lawyerprofile'):
+            lawyer_profile = self.lawyerprofile
+        return lawyer_profile
+    
+    def get_company_profile(self):
+        company_profile = None
+        if hasattr(self, 'companyprofile'):
+            company_profile = self.companyprofile
+        return company_profile
 
     def get_full_name(self):
-        full_name = '%s %s' % (self.first_name, self.last_name)
-        return full_name.strip()
+        return self.name
 
     def get_short_name(self):
-        return self.first_name
+        first_name = self.name.split(" ")
+        return first_name[0]
 
     def email_user(self, subject, message, from_email=None):
         send_mail(subject, message, from_email, [self.email])
+
+class LawyerProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    telephone = models.CharField(_('telephone'), max_length=30)
+    cpf = models.CharField(_('cpf'), max_length=15)
+
+class CompanyProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    activity = models.CharField(_('activity'), max_length=100)
